64b9105d034731c39fb6f4891acfcad1fe61d6b8
Steve-win11-main <liunandri@gmail.com>
2025-08-25 18:07:27 +0200

    use TStr::from_path

--- rinb/src/main.rs
+++ rinb/src/main.rs
@@ -4,8 +4,6 @@ mod esd_downloader;
 use esd_downloader::WinEsdDownloader;
 use wimlib::{string::TStr, OpenFlags, WimLib};
 
-use widestring::{error::NulError, U16CString};
-
 use std::fs;
 
 use clap::Parser;
@@ -19,14 +17,6 @@ struct Args {
     config: String,
     #[arg(long, default_value = "./.rinbcache/esd_cache")]
     cache_path: String,
-}
-
-pub fn tstr_from_str(s: &str) -> Result<Box<TStr>, NulError<u16>> {
-    // Convert &str → UTF-16 nul-terminated CString
-    let u16_cstring = U16CString::from_str(s)?;
-    let boxed = u16_cstring.into_boxed_ucstr();
-    // Safety: U16CStr and TStr are repr(transparent)
-    Ok(unsafe { Box::from_raw(Box::into_raw(boxed) as *mut TStr) })
 }
 
 fn main() -> anyhow::Result<()> {
@@ -56,7 +46,7 @@ fn main() -> anyhow::Result<()> {
 
     let wiml = WimLib::default();
     
-    let wimf = wiml.open_wim(&tstr_from_str(esd.to_str().unwrap()).unwrap(), OpenFlags::WRITE_ACCESS)?;
+    let wimf = wiml.open_wim(&TStr::from_path(esd).unwrap(), OpenFlags::WRITE_ACCESS)?;
     let xml = wimf.xml_data()?;
     let xml_str = xml.to_string()?;
     print!("{}",xml_str);
